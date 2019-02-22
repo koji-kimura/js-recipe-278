13cccbb7e9ced14e6ad4cc6f8ef5bb6dfb49a41c
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-02-22 21:50:53 +0900

    本日作業分

---
+++ src/recipe-79.js
@@ -0,0 +1,5 @@
+//　型を変換したい
+
+const result = 100 + number("200");
+
+console.log(result);
---
+++ src/recipe-80.js
@@ -0,0 +1,11 @@
+let a;
+console.log(a);
+
+const object = {};
+console.log(object.b);
+
+function myFunction(c) {
+  console.log(`bの値は${c}`);
+}
+
+myFunction();
