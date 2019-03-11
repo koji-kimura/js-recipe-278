c40a0470f08693918fdc2a2db8d75c2a96889d7a
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-03-11 22:01:32 +0900

    本日勉強分

---
+++ src/chap7/recipe-126.js
@@ -0,0 +1,9 @@
+// タッチ操作時のイベントの発生情報を取得したい
+// 自前のフリック処理を実現するとき
+// タッチ位置に要素を動かすとき
+
+const box = document.querySelector(".box");
+
+box.addEventListener("touchstart", event => {
+  // タッチ情報のリスト
+});
