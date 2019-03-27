85c0813064803ecccb002716c0105e43a46d4111
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-03-27 22:19:11 +0900

    本日勉強分
    フォームの操作についていろいろ

---
+++ src/chap9/recipe-161.js
@@ -0,0 +1,7 @@
+// テキストボックスの情報を取得したい
+// HTMLフォームの値を取得したい時
+// テキスト入力欄の値を書き換えたい時
+
+const element = document.querySelector("#myText");
+const value = element.value;
+console.log(value);
---
+++ src/chap9/recipe-163.js
@@ -0,0 +1,9 @@
+// テキストエリアの情報を取得したい
+// テキストエリアの文字列を取得したい時
+// テキストエリアの文字列を書き換えたい時
+
+// textareaの参照
+const element = document.querySelector("#myText");
+// 値を取得
+const value = element.value;
+console.log(value);
