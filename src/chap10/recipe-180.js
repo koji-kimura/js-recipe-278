45326923c1017becdd4ffb59f33bb718f8f6bc84
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-04-05 22:35:13 +0900

    本日勉強分
    アニメーション系

--- src/chap10/recipe-180.js
+++ src/chap10/recipe-180.js
@@ -1,3 +1,14 @@
 // 180 jsからcss TransitionsとcssAnimationを使いから
 
 // cssのアニメーションのタイミングに合わせて処理したい時;
+const button = document.querySelector("button");
+button.addEventListener("click", handleClick);
+
+function handleClick() {
+  const element = document.querySelector(".target");
+  if (element.classList.contains("state-show") === false) {
+    element.classList.add("state-show");
+  } else {
+    element.classList.remove("state-show");
+  }
+}
---
+++ src/chap10/reicpe-181.js
@@ -0,0 +1,7 @@
+// cssTransitionsの終了時に処理を行いたい
+
+const element = document.querySelector(".reat");
+
+element.addEventListener("transitionend", event => {
+  // アニメーション完了時のコード
+});
