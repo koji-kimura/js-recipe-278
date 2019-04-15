141c7a3bed1634ae56518b51ecc8b6e4c1a54ee6
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-04-15 22:29:37 +0900

    本日勉強分
    ジェネレータについて

--- src/chap19/recipe-274.js
+++ src/chap19/recipe-274.js
@@ -1,19 +1,54 @@
-// イテレータを自作したい
-// ジェネレータ
-// オリジナルのイテレータを定義したい時
+// ジェネレータの定義
 
-// イテレータを簡単に取り扱うための方法が「ジェネレータ」
-
-function* myGenerator() {
-  yield "鈴木";
-  yield "田中";
-  yield "後藤";
+function* range(start, end) {
+  let result = start;
+  while (result <= end) {
+    yield result;
+    result++;
+  }
+}
+// 使用例
+// ジェネレータの利用例として指定の範囲の整数を人ずつ返すイテレータの作例
+//while()内のyieldが実行されるたびに関数が一時停止してnext()が呼ばれると再会する
+for (let value of range(2, 6)) {
+  console.log(value);
 }
 
-const myIterable = myGenerator();
+// function* myGenerator() {
+//   console.log("こんにちは");
+//   yield 1000;
+//   console.log("今日はいい天気ですね");
+//   yield 2000;
+//   console.log("明日は晴れるでしょう");
+//   yield 3000;
+// }
 
-// next()で値を１つずつ取り出す
-console.log(myIterable.next().value);
-console.log(myIterable.next().value);
-console.log(myIterable.next().value);
-console.log(myIterable.next().done);
+// const myIterable = myGenerator();
+
+// setInterval(() => {
+//   console.log(myIterable.next().value);
+// }, 1000);
+
+// // // イテレータを自作したい
+// // // ジェネレータ
+// // // オリジナルのイテレータを定義したい時
+
+// // // イテレータを簡単に取り扱うための方法が「ジェネレータ」
+
+// // function* myGenerator() {
+// //   yield "鈴木";
+// //   yield "田中";
+// //   yield "後藤";
+// // }
+
+// // const myIterable = myGenerator();
+
+// // // // next()で値を１つずつ取り出す
+// // // console.log(myIterable.next().value);
+// // // console.log(myIterable.next().value);
+// // // console.log(myIterable.next().value);
+// // // console.log(myIterable.next().done);
+
+// // for (let value of myIterable) {
+// //   console.log(value);
+// // }
